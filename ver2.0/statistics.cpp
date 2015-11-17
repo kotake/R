fe3242382549900d569c8c51df9f0a5393a8a7bf
root <shingo014@gmail.com>
2015-11-17 19:37:42 +0900

    mpi追加しようかなってとこ

--- ver2.0/statistics.cpp
+++ ver2.0/statistics.cpp
@@ -1,4 +1,8 @@
 #include "head.hpp"
+#include <mpi.h>
+
+//#include <thread>
+//#include<//usr/lib64/R/library/RInside/include/RInside.h>
 
 //using namespace std;
 
@@ -7,11 +11,12 @@ static const double INF = 1000000000.0;
 unsigned int index = 0,row_num=0,col_num=0;
 double *csv_data;
 string *rabel;
-const int K = 2; //クラスタ数
+const int K = 3; //クラスタ数
 int sheet_num = 0;
 int rab_x=0, rab_y=0;
 double SD[K];
 vector<int> order;
+int level;
 
 int csv_read(const char *filename);
 vector<double> tovec();
@@ -23,15 +28,19 @@ void sokan(const vector<P> &input);
 void aso(double *input);
 
 
-ofstream fout;//sokan出力用
+ofstream sokanf;//sokan出力用
+ofstream sdf;//k-meansのクラスタごとの標準偏差の出力用
 
 int main(){
 	const char *fn = "iris.csv";
 
 	vector<double> csv;
+	
+	cout << "fillerレベルを入力してください(1-10)"<<endl;
+	cin >> level;
 
 	csv_read(fn);
-	cout<< "csv読み込み完了"<< endl;
+	//cout<< "csv読み込み完了"<< endl;
 	csv.reserve(index);
 
 	//ベクター型のcsvというコンテナに入力データcsv_data[]を入れる
@@ -68,16 +77,18 @@ int main(){
 		cout << "フォルダcorrelationを作成しました"<<endl;
 	};
 	output_name<< "correlation/output.txt";
-	fout.open(output_name.str());
+	sokanf.open(output_name.str());
 
-
+	sdf.open("k-means/standard_deviation.txt");
 
 	do{
 		//cout << "[ " << order[0];
 		for(unsigned int i=0 ; i < r ; i = i + 2){
 
-			cout << "[ "<<order[i] << "列目と"<<order[i+1]<< "列目 ]" <<endl;
+			//cout << "[ "<<order[i] << "列目と"<<order[i+1]<< "列目 ]" <<endl;
 
+			sokanf << "[ "<<order[i] << "列目と"<<order[i+1]<< "列目 ]" <<endl;
+			sdf << "[ "<<order[i] << "列目と"<<order[i+1]<< "列目 ]" <<endl;
 
 			//data[行数]に全組み合わせで値入れる
 			for(int j = 0 ; j < row_num ; j++){
@@ -87,22 +98,25 @@ int main(){
 
 			//ここからdata[]に対して3つの統計処理を行う
 			//kmeans関数内でgnuplotまでやってる
-			rab_x=order[i];
+			rab_x=order[i];//gnuplotで使うrabelの添え字
 			rab_y=order[i+1];
-			sokan(data);
-			kmeans(data);
+			if(myid == 0)	sokan(data);
+			else if(myid == 1)	kmeans(data);
 
 		}
-		cout <<endl;
+		//cout <<endl;
 		sheet_num++;
 	}while(next_combination(order.begin(),order.begin()+r, order.end()));//csv[]の中の順番が変わる
 
 	cout <<"-----------------------------------------"<<endl;
 	cout << "相関とk-means終了"<<endl<<endl;
 
-	aso(csv_data);
+	else if(myid == 2)	aso(csv_data);
 
-	fout.close();
+	sokanf.close();
+	sdf.close();
+
+	cout << "Complete!!!" <<endl;
 
 	delete [] csv_data;
 	return 0;
@@ -143,11 +157,17 @@ void gnuplot()
 	fprintf(gnuplot, "\n");
 	fprintf(gnuplot, "exit");
 	fflush(gnuplot); //バッファを書き出す
-	cout << "result" << sheet_num << ".png書き込み完了"<<endl;
+	pclose(gnuplot);
+	//cout << "result" << sheet_num << ".png書き込み完了"<<endl;
+	/*for(int i=0;i<K;i++){
+	  printf("index %d",i);
+	  if(i!=K-1) printf(", \"\" ");
+	  }*/
+	//this_thread::sleep_for(chrono::seconds(1));
 }
 
 void sokan(const vector<P> &input){
-	cout << "sokan start"<<endl;
+	//cout << "sokan start"<<endl;
 
 	double r=0;
 	double x_avg=0,y_avg=0;
@@ -162,7 +182,7 @@ void sokan(const vector<P> &input){
 	}
 	y_avg = accumulate(tmp_y.begin(),tmp_y.end(),0.0)/(double)row_num;
 
-	fout << x_avg <<","<< y_avg << endl;
+	//sokanf << x_avg <<","<< y_avg << endl;
 
 	for(int i =0;i < row_num;i++){
 		chi += (input[i].x - x_avg) * (input[i].y - y_avg);
@@ -170,13 +190,13 @@ void sokan(const vector<P> &input){
 		mo2 += pow((input[i].y - y_avg),2.0);
 	}
 	r = chi / (sqrt(mo1)*sqrt(mo2));
-	fout <<"相関係数:"<<  r << endl<<endl;
+	sokanf <<"相関係数:"<<  r << endl<<endl;
 }
 
 //エントリポイント
 void kmeans(const vector<P> &input){
 
-	cout << "kmeans start"<<endl;
+	//cout << "kmeans start"<<endl;
 
 	//Kmeansによるクラスタリング
 	vector<int> prev_cluster, cluster; //各点のクラスタ番号
@@ -278,11 +298,14 @@ void kmeans(const vector<P> &input){
 		}
 	}
 
-	fout.open("k-means/standard_deviation.txt");
+	//ofstream sdf;
+	//sdf.open("k-means/standard_deviation.txt");
+	
 	for(int i=0;i<K;i++){
-		fout << "クラスタ["<<i<<"]の標準偏差:"<< SD[i] << endl;
+		sdf << "クラスタ["<<i<<"]の標準偏差:"<< SD[i] << endl;
 	}
-	fout.close();
+	sdf << endl;
+	//fout.close();
 
 	gnuplot();
 
@@ -375,6 +398,7 @@ vector<double> tovec()
 
 void aso(double *input){
 
+	cout << "aso start"<<endl;
 	//岡システム
 	double sum,avg;
 	if(mkdir("association",777)==0){
@@ -398,15 +422,36 @@ void aso(double *input){
 	ofs2.close();
 
 
-	FILE *R = popen("R --vanilla --save --slave","w");
+	FILE *R = popen("R --vanilla --slave","w");
+	//FILE *R = popen("R","w");
+	//cout << "R起動中"<<endl;
 	fprintf(R, "suppressMessages(library(\"arules\"))\n");
 	fprintf(R, "iris.tran<-read.transactions(file='association/transaction.txt',sep=',',format='basket')\n");
+	//cout << "計算中"<<endl;
 	fprintf(R, "invisible(capture.output(iris.ap<-apriori(iris.tran)))\n");
 	fprintf(R, "sink(file = \"association/foo.txt\")\n");
+	//cout << "foo.txt作成"<<endl;
 	fprintf(R, "suppressWarnings(inspect(head(SORT(iris.ap, by = \"support\"),n=6)))\n");
 	fprintf(R, "sink()\n");
 	fprintf(R, "q(\"no\")\n");
 	fflush(R);
-	pclose(R);
+	//pclose(R);
 
 }
+
+
+
+//1変数を受け取り、フィルターを通る実行結果のみをfillterフォルダに出力
+void fillter(double var){
+	if(mkdir("fillter",777)==0){
+		cout << "フォルダfillerを作成しました" << endl;
+		mkdir("fillter/correlation",777);
+		mkdir("fillter/k-means",777);
+		mkdir("fillter/association",777);
+	}
+
+	var;
+//	ofstream fillter_f("fillter/correlation/");
+
+}
+
